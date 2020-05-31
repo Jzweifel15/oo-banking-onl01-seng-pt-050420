a0e670950863b90f5411bb62d52032f20bc7590b
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-05-31 12:44:44 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,9 +20,8 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else
-      @sender.balance -= @amount 
-      @receiver.balance += @amount
-      @status = "complete"
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
     end
   end
   
